3bb90799438ed9f05282178fa92a1d8243eed815
sionziom <adriansionek95@gmail\.com>
2020-07-01 22:09:01 +0200

    badges

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,11 +1,16 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,13 +29,23 @@ public class TrelloController {
         try {
             List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-            trelloBoards.stream()
-                    .filter(trelloBoardDto -> trelloBoardDto.getId() != null & trelloBoardDto.getName() != null)
-                    .filter(trelloBoardDto -> trelloBoardDto.getName().contains("Course"))
-                    .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+            trelloBoards.forEach(trelloBoardDto -> {
+                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                System.out.println("This board contains lists: ");
+                trelloBoardDto.getLists().forEach(trelloListDto ->
+                        System.out.println(trelloListDto.getName() + " - " + trelloListDto.getId() + " - " + trelloListDto.getIsClosed()));
+            });
+
+
         } catch (BoardsNotFoundException e) {
             System.out.println("Boards not found" + e);
         }
 
     }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
+        return trelloClient.createNewCard(trelloCardDto);
+    }
+
 }
