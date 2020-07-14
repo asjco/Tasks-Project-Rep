d9f0662a55af4be6ae085d3888e5ad532f2d8729
sionziom <adriansionek95@gmail\.com>
2020-07-14 18:18:46 +0200

    Scheduler

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,40 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import com.crud.tasks.trello.config.AdminConfig;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+
+    private SimpleEmailService simpleEmailService;
+    private TaskRepository taskRepository;
+    private AdminConfig adminConfig;
+
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    public EmailScheduler(SimpleEmailService simpleEmailService, TaskRepository taskRepository, AdminConfig adminConfig) {
+        this.simpleEmailService = simpleEmailService;
+        this.taskRepository = taskRepository;
+        this.adminConfig = adminConfig;
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void sendInformationEmail() {
+
+        long size = taskRepository.count();
+        String nameBySize = " tasks";
+        if (size == 1) {
+            nameBySize = " task";
+        }
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                "Currently in database you got: " + size + nameBySize,
+                null)
+        );
+    }
+}
--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -23,7 +23,7 @@ class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail(){
         //Given
-        Mail mail = new Mail("test@test.com", "Test", "Test message");
+        Mail mail = new Mail("test@test.com", "Test", "Test message", null);
 
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(mail.getMailTo());
